2ee6359555ff1e97813c399ca14ac9291df3833e
Cypherfelix <felixmarvinonline@gmail.com>
2023-08-19 19:11:08 +0300

    style(): changed the footer background color

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="bg-slate-800 text-white text-center py-3 w-full">
+    <footer className="bg-transparent text-white text-center py-3 w-full">
       <p className="text-sm">
         © {new Date().getFullYear()} WezaCare. All rights reserved.
       </p>
